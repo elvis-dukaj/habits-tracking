91d8b958bd12913b351850fe142e10dfb4270da4
Elvis Dukaj <elvis.dukaj@iu-study.org>
2023-07-05 11:44:09 +0200

    renaming habit_cli to habit_frontend

--- habit_frontend/setup.py
+++ habit_frontend/setup.py
@@ -6,7 +6,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
-        'Click', 'plotext'
+        'Click', 'plotext', 'tabulate'
     ],
     entry_points={
         'console_scripts': [
