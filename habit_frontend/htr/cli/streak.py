d23efd3b9c91dcb7ed41a28fc94091ce867a71ce
Elvis Dukaj <elvis.dukaj@iu-study.org>
2023-07-20 08:49:11 +0200

    working on analytics

--- habit_frontend/htr/cli/streak.py
+++ habit_frontend/htr/cli/streak.py
@@ -1,17 +1,44 @@
 import datetime
+import itertools
 from typing import Optional
 import click
 import plotext as plot
 from operator import attrgetter
 from collections import deque
 from itertools import islice
+import numpy
+
+"""
+Philipp and John (privacy), Kevin (Security Team)
+1st privancy
+than security
+
+transparency with the user (sensitivity is low)
+ -- user agreement
+ -- this is requiring a opt-out
+ 
+ 
+-- VRX:
+ -- we cannot do opt-out
+  
+  
+-- draft on how it works
+
+Spaces Connect:
+ SpacesConnect is How to connect with Spaces Services
+
+
+23.7 targeted release
+
+
+"""
 
 from htr.cli.cli import cli
 from htr.client.habit_tracker import HabitTrackerClient, Habit, HabitEvent
 
 
 @cli.group()
-@click.option("--user-id", "user_id", type=click.INT, envvar="HABIT_TRACKER_USER_ID", default=1,
+@click.option("--user-id", type=click.INT, envvar="HABIT_TRACKER_USER_ID", default=1,
               help="filter by habit id")
 @click.pass_obj
 def streak(habit_tracker_client: HabitTrackerClient, user_id: int):
@@ -47,14 +74,46 @@ def calculate_streaks(events: list[HabitEvent], periodicity: int):
     return streaks
 
 
-def calculate_all_streaks(habits: list[tuple[Habit, list[HabitEvent]]]) -> tuple[list[str], list[int]]:
-    tasks: list[str] = [habit.task for habit, events in habits]
+def sequential_difference(events: list[HabitEvent]):
+    streaks: list[int] = []
+
+    for window in sliding_window_iter(events, 2):
+        current = window[0]
+        previous = window[1]
+
+        delta: datetime.timedelta = current.completed_at - previous.completed_at
+        delta_in_days: int = delta.days
+
+        streaks.append(delta_in_days)
+
+    print(streaks)
+    return streaks
+
+
+def calculate_streak_history(elements: list[HabitEvent], periodicity: int):
+    # diffs = sequential_difference(elements)
+
+    print(periodicity)
+    diffs: list[int] = [1, 1, 1, 2, 1, 3, 1, 1]
+
+    previous_element: int = diffs[0]
+    current_streaks = 0
+
+    streak_list = [1 if elem < periodicity else 0 for elem in diffs]
+
+    streak_history = [len(list(group)) for _, group in itertools.groupby(streak_list)]
+
+    print("current streak_list: ", streak_list)
+    print("streak_history: ", streak_history)
+
+
+def calculate_all_streaks(habits: list[tuple[Habit, list[HabitEvent]]]) -> list[int]:
     streaks: list[int] = []
 
     for habit, events in habits:
         streaks.append(calculate_streaks(events, habit.periodicity))
 
-    return tasks, streaks
+    return streaks
 
 
 @streak.command()
@@ -75,14 +134,24 @@ def display(habit_tracker_client: HabitTrackerClient, habit_id: Optional[int]):
         events = habit_tracker_client.list_habit_events(habit.habit_id)
         combined_habit_and_events.append((habit, events))
 
-    histogram = calculate_all_streaks(combined_habit_and_events)
+    tasks: list[str] = [habit.task for habit in habits]
+    streaks = calculate_all_streaks(combined_habit_and_events)
 
-    plot.simple_bar(histogram[0], histogram[1], title="progress of current streaks")
+    plot.simple_bar(tasks, streaks, title="progress of current streaks")
     plot.show()
 
 
 @streak.command()
-@click.option("--habit-id", required=True, type=click.INT, help="filter by habit id")
+@click.option("--with-habit-id", "habit_id", type=click.INT, help="filter by habit id")
 @click.pass_obj
-def longest(habit_tracker_client: HabitTrackerClient, habit_id: int):
-    click.echo(f"longest streak for {habit_id} is 2")
+def longest(habit_tracker_client: HabitTrackerClient, habit_id: Optional[int]):
+    if habit_id is None:
+        habit_id = 3
+
+    habit = habit_tracker_client.get_habit_by_id(habit_id)
+    events: list[HabitEvent] = habit_tracker_client.list_habit_events(habit.habit_id)
+    streaks = calculate_streaks(events, habit.periodicity)
+
+    calculate_streak_history(events, 3)
+    click.echo(f"Habit {habit_id} has {streaks} streaks")
+    return
