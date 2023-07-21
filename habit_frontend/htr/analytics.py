85a72aa9d3dc5b1468b0b740dfbae65a19bc5350
Elvis Dukaj <elvis.dukaj@iu-study.org>
2023-07-21 15:33:43 +0200

    Refactoring

--- habit_frontend/htr/analytics.py
+++ habit_frontend/htr/analytics.py
@@ -3,17 +3,20 @@ import numpy as np
 from htr.schemas.habit_event import HabitEvent
 
 
-def calculate_record_streak(events: list[HabitEvent], periodicity: int) -> int:
-    completed_events: np.ndarray = np.array([event.completed_at for event in events], dtype=np.datetime64)
+def get_delta_times_grouped_by_consecutive_streak(completed_events: np.ndarray):
     partial_diff = np.ediff1d(completed_events).astype(np.int32)
     second_partial_diff = np.ediff1d(partial_diff)
-    grouped_by_periodicity = np.split(partial_diff, np.where(second_partial_diff != 0)[0] + 1)
+    return np.split(partial_diff, np.where(second_partial_diff != 0)[0] + 1)
+
+
+def calculate_record_streak(events: list[HabitEvent], periodicity: int) -> int:
+    completed_events: np.ndarray = np.array([event.completed_at for event in events], dtype=np.datetime64)
+    grouped_by_periodicity = get_delta_times_grouped_by_consecutive_streak(completed_events)
 
     record_strike = 0
     for g in grouped_by_periodicity:
         # check if the streak was broken
         if g[0] > periodicity:
-            print(f"the periodicity {g[0]} was bigger than {periodicity}, not considering for the streak")
             continue
 
         record_strike = max(record_strike, g.shape[0])
