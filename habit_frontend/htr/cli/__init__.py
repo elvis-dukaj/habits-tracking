7ca72327a2dd9455360f05d8fb25375f07c538f1
Elvis Dukaj <elvis.dukaj@iu-study.org>
2023-07-25 19:56:21 +0200

    Replacing streak command line with statistic. Now it's possible to view all the statistics and order the data

--- habit_frontend/htr/cli/__init__.py
+++ habit_frontend/htr/cli/__init__.py
@@ -1,4 +1,4 @@
 from htr.cli.habit import habit as cli_habit
 from htr.cli.user import user as cli_user
-from htr.cli.streak import streak as cli_streak
+from htr.cli.statistic import statistic as cli_statistic
 from htr.cli.cli import cli
--- habit_frontend/htr/cli/statistic.py
+++ habit_frontend/htr/cli/statistic.py
@@ -1,109 +1,76 @@
-# from typing import Optional
-#
-# import pandas
-# from tabulate import tabulate
-# import click
-# import plotext as plot
-#
-# from htr.cli.cli import cli
-# from htr.client.habit_tracker import HabitTrackerClient
-# from htr.schemas.habit import Habit
-# from htr.schemas.habit_event import HabitEvent
-#
-# from htr.analytics import transform_to_panda_dataframe, get_habit_events_statistic, tabulate_dataframe
-#
-#
-# @cli.group()
-# @click.option("--user-id", type=click.INT, envvar="HABIT_TRACKER_USER_ID", help="filter by habit id")
-# @click.pass_obj
-# def statistic(habit_tracker_client: HabitTrackerClient, user_id: int):
-#     habit_tracker_client.set_current_user_id(user_id)
-#
-#
-# def calculate_all_streaks(habits: list[tuple[Habit, list[HabitEvent]]]) -> list[int]:
-#     dataframes: list[pandas.DataFrame] = []
-#
-#     for habit, events in habits:
-#         dataframes.append(transform_to_panda_dataframe(events, habit.periodicity))
-#
-#     streaks: list[int] = [0 if len(df.index) == 0 else df.iloc[-1]['streak'] for df in dataframes]
-#     return streaks
-#
-#
-# @statistic.command()
-# @click.option("--habit-id", "habit_id", required=True, type=click.INT, help="filter by habit id")
-# @click.pass_obj
-# def statistic(habit_tracker_client: HabitTrackerClient, habit_id: Optional[int], show_history: Optional[bool],
-#               show_statistic: Optional[bool]):
-#     pass
-#
-#
-# @streak.command()
-# @click.option("--with-habit-id", "habit_id", is_flag=False, type=click.INT, help="filter by habit id")
-# @click.pass_obj
-# def history(habit_tracker_client: HabitTrackerClient, habit_id: Optional[int], show_history: Optional[bool],
-#             show_statistic: Optional[bool]):
-#
-#
-# @streak.command()
-# @click.option("--with-habit-id", "habit_id", is_flag=False, type=click.INT, help="filter by habit id")
-# @click.option("--show-history", is_flag=True, default=True, help="filter by habit id")
-# @click.option("--show-statistic", is_flag=True, default=True, help="filter by habit id")
-# @click.pass_obj
-# def display(habit_tracker_client: HabitTrackerClient, habit_id: Optional[int], show_history: Optional[bool],
-#             show_statistic: Optional[bool]):
-#     if habit_id is not None:
-#         habit = habit_tracker_client.get_habit_by_id(habit_id)
-#         events: list[HabitEvent] = habit_tracker_client.list_habit_events(habit.habit_id)
-#         dataframe = transform_to_panda_dataframe(events, habit.periodicity)
-#
-#         if len(dataframe.index) == 0:
-#             click.echo("No data registered")
-#             return
-#
-#         if show_history:
-#             click.echo("Streak History:")
-#             click.echo(tabulate_dataframe(dataframe))
-#
-#         if show_statistic:
-#             click.echo("Streak Statistics:")
-#             click.echo(tabulate_dataframe(get_habit_events_statistic(dataframe)))
-#         return
-#
-#     combined_habit_and_events: list[tuple[Habit, list[HabitEvent]]] = []
-#
-#     habits = habit_tracker_client.list_habits()
-#     for habit in habits:
-#         events = habit_tracker_client.list_habit_events(habit.habit_id)
-#         combined = (habit, events)
-#         combined_habit_and_events.append(combined)
-#
-#     tasks: list[str] = [habit.task for habit in habits]
-#     current_streak = calculate_all_streaks(combined_habit_and_events)
-#
-#     plot.simple_bar(tasks, current_streak, title="progress of current streaks")
-#     plot.show()
-#
-#
-# @streak.command()
-# @click.pass_obj
-# def longest(habit_tracker_client: HabitTrackerClient):
-#     habits = habit_tracker_client.list_habits_by_user_id()
-#
-#     longest_habit: Optional[Habit] = None
-#     longest_streak: int = 0
-#
-#     for habit in habits:
-#         events = habit_tracker_client.list_habit_events(habit.habit_id)
-#         df = transform_to_panda_dataframe(events, habit.periodicity)
-#         current_longest = df.max()['streak']
-#         if longest_streak < current_longest:
-#             longest_habit = habit
-#             longest_streak = current_longest
-#
-#     if not longest_habit:
-#         click.echo("No records")
-#     else:
-#         click.echo(f"Habit '{longest_habit.task}' has the longest streak with {longest_streak} streaks")
-#
-#     return
+from typing import Optional
+
+import pandas
+import click
+import pandas as pd
+
+from htr.cli.cli import cli as htr_cli
+from htr.client.habit_tracker import HabitTrackerClient
+from htr.schemas.habit import Habit
+from htr.schemas.habit_event import HabitEvent
+
+from htr.analytics import transform_to_panda_dataframe, get_habit_events_statistic, tabulate_dataframe
+
+
+@htr_cli.group()
+@click.option("--user-id", type=click.INT, envvar="HABIT_TRACKER_USER_ID", help="filter by habit id")
+@click.pass_obj
+def statistic(habit_tracker_client: HabitTrackerClient, user_id: int):
+    habit_tracker_client.set_current_user_id(user_id)
+
+
+@statistic.command(name="all")
+@click.option("--order-by", type=click.Choice(['habit', 'current', 'longest', 'mean', 'periodicity']), default='habit')
+@click.option("--ascending", is_flag=True, default=True)
+@click.pass_obj
+def show_all(habit_tracker_client: HabitTrackerClient, order_by: str, ascending: bool):
+    habits = habit_tracker_client.list_habits()
+
+    habit_id_series = pandas.Series([habit.habit_id for habit in habits], name="habit id")
+    tasks = pandas.Series([habit.task for habit in habits], name="Task")
+    periodicities = pandas.Series([habit.periodicity for habit in habits], name="Periodicity")
+
+    current_streaks_list: list[int] = []
+    longest_streaks_list: list[int] = []
+    average_streaks_list: list[float] = []
+
+    for habit in habits:
+        events = habit_tracker_client.list_habit_events(habit.habit_id)
+        if len(events) > 0:
+            df = transform_to_panda_dataframe(events, habit.habit_id)
+            stat = get_habit_events_statistic(df)
+            current_streaks_list.append(stat['Last'][stat.last_valid_index()])
+            longest_streaks_list.append(stat['Longest'][stat.last_valid_index()])
+            average_streaks_list.append(stat['Mean'][stat.last_valid_index()])
+        else:
+            current_streaks_list.append(0)
+            longest_streaks_list.append(0)
+            average_streaks_list.append(0)
+
+    current_streaks: pandas.Series = pandas.Series(current_streaks_list)
+    longest_streaks: pandas.Series = pandas.Series(longest_streaks_list)
+    average_streaks: pandas.Series = pandas.Series(average_streaks_list)
+
+    df = pandas.DataFrame({
+        "Habit ID": habit_id_series,
+        "Task": tasks,
+        "Periodicity": periodicities,
+        "Current Streak": current_streaks,
+        "Longest Streak": longest_streaks,
+        "Average Streak": average_streaks,
+    })
+
+    if order_by == 'current':
+        click.echo(tabulate_dataframe(df.sort_values(by=['Current Streak'], ascending=ascending)))
+
+    if order_by == 'longest':
+        click.echo(tabulate_dataframe(df.sort_values(by=['Longest Streak'], ascending=ascending)))
+
+    if order_by == 'mean':
+        click.echo(tabulate_dataframe(df.sort_values(by=['Average Streak'], ascending=ascending)))
+
+    if order_by == 'periodicity':
+        click.echo(tabulate_dataframe(df.sort_values(by=['Periodicity'], ascending=ascending)))
+
+    if order_by == 'habit':
+        click.echo(tabulate_dataframe(df.sort_values(by=['Habit ID'], ascending=ascending)))
