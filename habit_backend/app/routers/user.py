923216414c9be5f9df7db2452300fa9a2131a24b
Elvis Dukaj <elvis.dukaj@iu-study.org>
2023-07-31 21:58:20 +0200

    Refactoring

--- habit_backend/app/routers/user.py
+++ habit_backend/app/routers/user.py
@@ -5,28 +5,28 @@ from app.schemas import UserCreate, UserRead
 
 
 def create_user_routers(habit_service: HabitsTrackingService) -> APIRouter:
-    routers = APIRouter(
+    router = APIRouter(
         prefix="/user",
         tags=["user"],
     )
 
-    @routers.post("/", response_model=UserRead, status_code=201)
+    @router.post("/", response_model=UserRead, status_code=201)
     def create(user: UserCreate):
         response = habit_service.create_user(user)
         return response
 
-    @routers.get("/id/{user_id}", response_model=UserRead)
+    @router.get("/id/{user_id}", response_model=UserRead)
     def get_by_id(user_id: int):
         user_response = habit_service.get_user_by_id(user_id)
         return user_response
 
-    @routers.get("/username/{username}", response_model=UserRead)
+    @router.get("/username/{username}", response_model=UserRead)
     def get_by_username(username: str):
         user_response = habit_service.get_user_by_username(username)
         return user_response
 
-    @routers.delete("/{user_id}")
+    @router.delete("/{user_id}")
     def delete(user_id: int):
         habit_service.delete_user(user_id)
 
-    return routers
+    return router
